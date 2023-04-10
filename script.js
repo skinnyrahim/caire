72b85ec824afb4b380267ecde61af4467fd7bfab
Abdulrahim Shaibu-Issah <1st.rahim@gmail.com>
2023-04-10 14:49:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-$(document).ready(function() {
-    $('#chat-form').submit(function(e) {
-        e.preventDefault();
-        var message = $('#chat-input').val();
-        $('#chat-input').val('');
-        $.ajax({
-            url: 'chatbot.php',
-            type: 'POST',
-            data: { message: message },
-            success: function(response) {
-                $('#chat-output').append('<p><strong>You:</strong> ' + message + '</p><p><strong>Chatbot:</strong> ' + response + '</p>');
-            }
-        });
-    });
+const chatForm = document.getElementById("chat-form");
+const chatInput = document.getElementById("chat-input");
+const chatOutput = document.getElementById("chat-output");
+
+chatForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const message = chatInput.value;
+	sendMessage(message);
 });
+
+function sendMessage(message) {
+	const xhr = new XMLHttpRequest();
+	xhr.open("POST", "/message");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onload = () => {
+		const response = JSON.parse(xhr.responseText);
+		updateChat(response.message);
+	};
+	const data = JSON.stringify({ message: message });
+	xhr.send(data);
+}
+
+function updateChat(message) {
+	const chatBubble = document.createElement("div");
+	chatBubble.classList.add("chat-bubble");
+	chatBubble.innerText = message;
+	chatOutput.appendChild(chatBubble);
+}
